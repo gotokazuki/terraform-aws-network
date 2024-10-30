9e4e7b49ccb7a7da7c8bc43636649417060ad2ea
gotokazuki <gotokazuki@pm.me>
2024-10-31 00:15:36 +0900

    change example values

--- examples/complete/locals.tf
+++ examples/complete/locals.tf
@@ -1,10 +1,10 @@
 locals {
-  prefix            = "hoge-dev"
+  prefix            = "test-dev"
   ipv4_cidr         = "10.0.0.0/16"
   ipv4_cidr_newbits = 3
   subnets_number    = 2
   default_tags = {
-    owner      = "gotokazuki"
+    owner      = "ownername"
     repository = "terraform-aws-network"
     created_by = "terraform"
   }
